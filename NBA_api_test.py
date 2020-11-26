49692c82ac541eb8bd293838488daf918084a75a
Luke <lukebmorrow@gmail.com>
2020-11-26 16:05:11 -0600

    Changed request to a dynamic request by league_id

--- NBA_api_test.py
+++ NBA_api_test.py
@@ -1,5 +1,6 @@
-from nba_api.stats.static import teams
+from nba_api.stats.endpoints import CommonTeamYears
 
-response = teams.get_teams()
+# https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/endpoints/commonteamyears.md
+response = CommonTeamYears(league_id="09")
 
-print(response)
+print(response.get_json())
