363a974c06f756d5248146d5b0d1baddc227802d
Luke <lukebmorrow@gmail.com>
2020-12-12 23:25:29 -0600

    Added comments

--- Calculations2.py
+++ Calculations2.py
@@ -19,16 +19,20 @@ with open("CalculationResults/bell_results.csv", 'w', newline='') as outfile:
         
         for k in range(len(TEAM_ABBR)):
             games = dataframe[dataframe['abbr'].str.contains(TEAM_ABBR[k])]
-            games = games[:82]#only use the first 83 games of each teams season
-            win_percent = "{:.3f}".format(int(games.iloc[len(games.index)-1]["wins"]) /(int(games.iloc[len(games.index)-1]["wins"]) + int(games.iloc[len(games.index)-1]["losses"]) ))
-            diff = (games['pts'] - games['opp_pts']).values.tolist()
-            avg_pts = games.sum(axis=0)['pts']
-            avg_allowed = games.sum(axis=0)['opp_pts']
+            games = games[:82]#only use the first 82 games of each teams season
+            win_percent = "{:.3f}".format(int(games.iloc[len(games.index)-1]["wins"]) /(int(games.iloc[len(games.index)-1]["wins"]) + int(games.iloc[len(games.index)-1]["losses"])))#calculates true win %
+
+            diff = (games['pts'] - games['opp_pts']).values.tolist()# Makes a list of (pts for - pts allowed) for each game
+
+            avg_pts = games.sum(axis=0)['pts']# sum of all points scored in a season
+            avg_allowed = games.sum(axis=0)['opp_pts']# sum of all points allowed in a season
 
             res = "{:.3f}".format(norm(scale=abs(avg_pts - avg_allowed)).cdf((avg_pts - avg_allowed) / (statistics.stdev(diff)))) #I improvised the scale number
-            results.append([TEAM_ABBR[k], str(res), str(win_percent)])
-            error+= abs(float(res)-float(win_percent))
 
-    print( "average error: "+ str("{:.3f}".format(error/len(results))))
-    csvwriter.writerows(results)
+            results.append([TEAM_ABBR[k], str(res), str(win_percent)])# build rows to put into outfile
+            error+= abs(float(res)-float(win_percent))#first step in error calculation
+
+    print( "average error: "+ str("{:.3f}".format(error/len(results))))# second step in error calculation and print said error calc
+
+    csvwriter.writerows(results)# write to csv file
 
